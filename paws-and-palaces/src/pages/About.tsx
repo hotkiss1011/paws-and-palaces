af1873f5d72a035114483e9b1372af6caf60e3df
Amanda Hotchkiss <amanda.hotchkiss1011@gmail.com>
2023-11-22 13:12:18 -0500

    added content

--- paws-and-palaces/src/pages/About.tsx
+++ paws-and-palaces/src/pages/About.tsx
@@ -1,7 +1,12 @@
 function About() {
     return (
         <div className="about">
-            <h1>About</h1>
+            <h2>About the Business</h2>
+            <p>At Paws & Palaces, we're not just pet care providers; we're your pets' second best friends. With a passion for paws and a commitment to creating joyful experiences, we offer a range of services designed to cater to your furry family members. From house-sitting that turns your home into a haven to playful dog walks and heartwarming drop-ins, we understand that pets are more than animals; they're beloved companions. Our experienced team, led by a former vet tech, brings a wealth of knowledge and care to every interaction. Trust us to make your pets feel cherished and your time away worry-free. Because at Paws & Palaces, it's not just a service; it's a celebration of the love we share with our four-legged friends.</p>
+            <br />
+            
+            <h2>About the Owner</h2>
+            <p>Meet the heart and soul behind Paws & Palaces, Brooke Sherlin. With a background as a skilled vet assistant and a household that harmonizes with the joyful barks and wagging tails of three beloved dogs, Brooke is not just a pet lover; she's a dedicated advocate for their well-being. Drawing on years of experience in veterinary care, she brings a unique blend of expertise and compassion to every service offered. Her commitment to creating a nurturing environment for pets stems not just from professional knowledge but from the genuine love she holds for her own canine companions. When you entrust your pets to Brooke and her team, you're choosing care that goes beyond duty – it's a reflection of the same love that fills her own home.</p>
         </div>
     );
 }
