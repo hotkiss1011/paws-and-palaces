107e5f5a14278308ca99b849b91a3b7672bfa228
Amanda Hotchkiss <amanda.hotchkiss1011@gmail.com>
2023-11-22 13:11:09 -0500

    updated name

--- paws-and-palaces/src/pages/Home.tsx
+++ paws-and-palaces/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="home">
-            <h1>Brooke Sherlin</h1>
+            <h1>Paws & Palaces</h1>
             <h2>
                 Where every wag is welcome!<br/>Your trusted house and dog sitter for a pawsitively stress-free experience.
             </h2>
